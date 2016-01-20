58987e027b606f40ca5e6847ef3a07f571b8837e
Isaac Madwed <i.madwed@gmail.com>
2016-01-20 13:28:11 -0500

    update package

--- android/src/main/java/RNLocationSettingsPackage.java
+++ android/src/main/java/RNLocationSettingsPackage.java
@@ -12,7 +12,7 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 
-public class RNLocationSettingsModule implements ReactPackage {
+public class RNLocationSettingsPackage implements ReactPackage {
 
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
